2b91aabc99b319090b56ba0349139df02a26ab65
DELL <DELL@DESKTOP-ULCFHFQ>
2024-10-17 22:23:26 +0530

    First Commit.

--- CucmberSelenium/src/test/java/StepDefinitions/GoogleSearchSteps.java
+++ CucmberSelenium/src/test/java/StepDefinitions/GoogleSearchSteps.java
@@ -14,6 +14,7 @@ import io.cucumber.java.en.When;
 
 public class GoogleSearchSteps {
 	WebDriver driver=null;
+	int a=0;
 	@Given("browser is open")
 	public void browser_is_open() {
 	    System.getProperty("webdriver.chrome.driver","E:/JavaPro/src/test/resources/driver/chromedriver.exe");
